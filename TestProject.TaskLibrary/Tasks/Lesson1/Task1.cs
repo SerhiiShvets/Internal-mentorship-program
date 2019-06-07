1dda46cbfce878802f17f4c4615f6a309ff9dd94
Serhii Shvets <sergius.shvets@gmail.com>
2019-06-07 14:51:03 +0300

    Lesson1, tasks 1,2

---
+++ TestProject.TaskLibrary/Tasks/Lesson1/Task1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson1
+{
+    public class Task1 : IRunnable
+    {
+        public void Run()
+        {     
+            var thousands = new List<string> { "M" };
+            var hundreds = new List<string> { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            var tens = new List<string> { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            var units = new List<string> { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+            var listsWithRomanValues = new List<List<string>>();
+            listsWithRomanValues.Add(units);
+            listsWithRomanValues.Add(tens);
+            listsWithRomanValues.Add(hundreds);
+            listsWithRomanValues.Add(thousands);
+
+            Console.WriteLine("Input a number to convert");
+            string arabicNumberToConvert = Console.ReadLine();
+            string convertedToRoman;
+            int checkedNumberToConvert;
+
+            if (Int32.TryParse(arabicNumberToConvert, out checkedNumberToConvert))
+            {
+                if (0 < checkedNumberToConvert && checkedNumberToConvert < 4000)
+                {
+                    convertedToRoman = ConvertToRoman(checkedNumberToConvert, listsWithRomanValues);
+                    Console.WriteLine(convertedToRoman);
+                }
+                else
+                {
+                    Console.WriteLine("Input is incorrect");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input is not an integer number");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string ConvertToRoman(int numberToConvert, List<List<string>> listsOfValuesForRoman)
+        {
+
+            int counter = 0;
+            string convertedDigit = "";
+            string numberToConvertString = numberToConvert.ToString();
+            List<string> valuesOfDigits;
+
+            for (int i = numberToConvertString.Length - 1; i >= 0; i--)
+            {
+                valuesOfDigits = listsOfValuesForRoman[counter];
+
+                for (int j = 0; j < valuesOfDigits.Count; j++)
+                {
+                    string tempString = Convert.ToString(numberToConvertString[i]);
+                    int tempInt = Convert.ToInt32(tempString);
+
+                    if (tempInt == j)
+                    {
+                        convertedDigit = valuesOfDigits[j] + convertedDigit;
+                    }
+                }
+                counter++;
+            }
+            return convertedDigit;
+        }
+    }
+
+}
---
+++ TestProject.TaskLibrary/Tasks/Lesson1/Task2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson1
+{
+    public class Task2 : IRunnable
+    {
+        public void Run()
+        {
+
+        }
+
+        public struct Rational : IComparable<Rational>, IEquatable<Rational>
+        {
+            int gcd;
+            public int Nominator { get; set; }
+            public int Denominator { get; set; }
+            public double quotient;
+            public string rationalNumber;
+            public string reductedRationalNumber;
+
+            public Rational(int nom, int denom)
+            {
+                if (denom != 0)
+                {
+                    Nominator = nom;
+                    Denominator = denom;
+                    quotient = nom / denom;
+                    rationalNumber = nom.ToString() + "/" + denom.ToString();
+                    gcd = GetGCD(nom, denom);
+                    reductedRationalNumber = (nom / gcd).ToString() + "/" + (denom / gcd).ToString();
+
+                }
+                else
+                {
+                    throw new ArgumentException("The denominator cannot be 0", "denom");
+                }
+
+            }
+            //GCD
+            static int GetGCD(int x, int y)
+            {
+                return y == 0 ? x : GetGCD(y, x % y);
+            }
+            //IComparable
+            public int CompareTo(Rational other)
+            {
+                return this.quotient.CompareTo(other.quotient);
+            }
+            //IEquatable
+            public bool Equals(Rational other)
+            {
+                if (this.quotient == other.quotient)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            //overrided ToString()
+            public override string ToString()
+            {
+                if (Denominator == 1)
+                {
+                    return Nominator.ToString();
+                }
+                else
+                {
+                    return this.Nominator + "r" + this.Denominator;
+                }
+            }
+            //Addition
+            public static Rational operator + (Rational r1, Rational r2)
+            {
+                int newNominator = r1.Nominator * r2.Denominator + r2.Nominator * r1.Denominator;
+                int newDenominator = r1.Denominator * r2.Denominator;
+                return new Rational(newNominator, newDenominator);
+            }
+            //Subtraction
+            public static Rational operator - (Rational r1, Rational r2)
+            {
+                int newNominator = r1.Nominator * r2.Denominator - r2.Nominator * r1.Denominator;
+                int newDenominator = r1.Denominator * r2.Denominator;
+                return new Rational(newNominator, newDenominator);
+            }
+            //Multiplication
+            public static Rational operator * (Rational r1, Rational r2)
+            {
+                int newNominator = r1.Nominator * r2.Denominator * r2.Nominator * r1.Denominator;
+                int newDenominator = r1.Denominator * r2.Denominator;
+                return new Rational(newNominator, newDenominator);
+            }
+            //Division
+            public static Rational operator / (Rational r1, Rational r2)
+            {
+                int newNominator = r1.Nominator * r2.Denominator;
+                int newDenominator = r1.Denominator * r2.Nominator;
+                return new Rational(newNominator, newDenominator);
+            }
+            //Negation
+            public static Rational operator - (Rational r1)
+            {
+                int newNominator = r1.Nominator * (-1);
+                return new Rational(newNominator, r1.Denominator);
+            }
+            //To treat integers like rationals
+            public static Rational ConvertIntToRational(int i)
+            {
+                return new Rational(i, 1);
+            }
+        }
+    }
+}
